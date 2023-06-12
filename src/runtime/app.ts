6af9b8a4ce0836e6e4efac571c092276e81af2e6
Hebilicious <xsh4k3@gmail.com>
2023-06-12 19:29:16 +0700

    tests: add form actions tests

--- src/runtime/app.ts
+++ src/runtime/app.ts
@@ -75,26 +75,27 @@ function createNitroApp(): NitroApp {
         fetchWithEvent(event, req as any, init, { fetch: $fetch });
     })
   );
-  
-  const renderer = handlers.find(({ route }) => route === "/**");
-  const rendererHandler = lazyEventHandler(renderer.handler);
 
-  const regularHandlers = handlers.filter((h) => !h.formAction);
   const formActionsHandlers = handlers.filter((h) => h.formAction);
+  const regularHandlers = handlers.filter((h) => !h.formAction);
 
-  for (const h of formActionsHandlers) {
-    let handler = h.lazy ? lazyEventHandler(h.handler) : h.handler;
-    const routeRules = getRouteRulesForPath(
-      h.route.replace(/:\w+|\*\*/g, "_")
-    );
-    if (routeRules.cache) {
-      handler = cachedEventHandler(handler, {
-        group: "nitro/routes",
-        ...routeRules.cache,
-      });
+  if (formActionsHandlers.length > 0) {
+    const renderer = handlers.find(({ route }) => route === "/**");
+    const rendererHandler = lazyEventHandler(renderer.handler);
+
+    for (const h of formActionsHandlers) {
+      let handler = h.lazy ? lazyEventHandler(h.handler) : h.handler;
+      const routeRules = getRouteRulesForPath(h.route.replace(/:\w+|\*\*/g, "_"));
+      if (routeRules.cache) {
+        handler = cachedEventHandler(handler, {
+          group: "nitro/routes",
+          ...routeRules.cache,
+        });
+      }
+      formActionsRouter.use(h.route, handler, "post");
+      formActionsRouter.use(h.route, rendererHandler, "get");
     }
-    formActionsRouter.use(h.route, handler, "post")
-    formActionsRouter.use(h.route, rendererHandler, "get");
+    h3App.use(config.app.baseURL, formActionsRouter);
   }
 
   for (const h of regularHandlers) {
@@ -119,7 +120,6 @@ function createNitroApp(): NitroApp {
     }
   }
 
-  h3App.use(config.app.baseURL, formActionsRouter);
   h3App.use(config.app.baseURL, router);
 
   const app: NitroApp = {
